65c35fbe7edac29e321a6af9593d32453c895e35
Alex Shopiak <alshop2004@gmail.com>
2024-04-17 19:09:29 +0000

    F

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -25,7 +25,12 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie('access_token', token, { httpOnly: true, sameSite:"None", secure:true })
+      .cookie('access_token', token, { 
+        httpOnly: true, 
+        sameSite:"None", 
+        secure:true,
+        maxAge: 2*60*60*1000,
+      })
       .status(200)
       .json(rest);
   } catch (error) {
@@ -40,7 +45,12 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-        .cookie('access_token', token, { httpOnly: true, sameSite:"None", secure:true })
+        .cookie('access_token', token, { 
+          httpOnly: true, 
+          sameSite:"None", 
+          secure:true,
+          maxAge: 2*60*60*1000,
+        })
         .status(200)
         .json(rest);
     } else {
@@ -60,7 +70,12 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = newUser._doc;
       res
-        .cookie('access_token', token, { httpOnly: true, sameSite:"None", secure:true })
+        .cookie('access_token', token, { 
+          httpOnly: true, 
+          sameSite:"None", 
+          secure:true,
+          maxAge: 2*60*60*1000,
+        })
         .status(200)
         .json(rest);
     }
